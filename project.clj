a4b0a029a44b66851d4267e15502241cc4fe9bed
Brian Picciano <bgp2396@bellsouth.net>
2012-11-17 13:38:30 -0500

    init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject sugar-glider "0.1.0"
+  :description "A way of executing a piece of code on a remote clojure instance and retrieving its result"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
